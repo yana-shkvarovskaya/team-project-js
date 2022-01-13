01724747c15ddb798f92b4ed197b78a97bd9bccf
Tetiana Legois <mme.tanyalegois@gmail.com>
2022-01-13 15:54:23 +0200

    1

--- src/js/components/get-popular.js
+++ src/js/components/get-popular.js
@@ -3,7 +3,9 @@ import getRefs from '../refs';
 import card from '../../templates/cardMovie';
 import createCardData from '../data/create-card-data';
 import { startSpinner, stopSpinner } from './preloader';
-// import renderPagination from './pagination';
+/* import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css'
+ */
 
 const { insertPoint } = getRefs();
 
@@ -33,3 +35,37 @@ async function createMarkup() {
 }
 
 createMarkup();
+
+/* const container = document.getElementById('pagination');
+const options = {
+  totalItems: 20000,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+const pagination = new Pagination(container, options);
+
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  console.log(currentPage);
+}); */
