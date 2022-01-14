e905819b40d769c3ae803a34f73c834088355df5
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2022-01-14 13:30:08 +0200

    edits

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,39 +1,17 @@
 import getRefs from '../refs';
 import createModalFilm from '../data/create-modal.data';
 import modal from '../../templates/modal.hbs';
-import getTrailer from './get-trailer';//добавляет ютуб ролик
+import getTrailer from './get-trailer'; //добавляет ютуб ролик
 // import { currentStorage, changeStorage } from './library';
-
-//firebase connect
-import { initializeApp } from 'firebase/app';//https://www.npmjs.com/package/firebase
-import {
-  getDatabase,
-  ref,
-  get,
-  set,
-  remove,
-} from 'firebase/database';
-const firebaseConfig = {
-apiKey: "AIzaSyCkGcqujxOrDVU9Olzs2GeAg5RVL6Rx6mU",
-authDomain: "project-6349d.firebaseapp.com",
-projectId: "project-6349d",
-storageBucket: "project-6349d.appspot.com",
-messagingSenderId: "1092606217949",
-appId: "1:1092606217949:web:f3c739bc5b564639f45534",
-measurementId: "G-YZR14G97E7"
-};
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-//firebase
 
 const {
   insertPoint,
-  //modalRef,
+  modalRef,
   modalСardRef,
   overlayBackgroundRef,
   overlayRef,
   clsBtnRef,
-  
+
   //libraryLink,
 } = getRefs();
 let movieID;
@@ -55,12 +33,12 @@ async function onClickOnCard(e) {
 
     //Добавляем данные фильма в LS для возможного добавления карточки в библиотеку
     addItemToLocalStorage(result);
-    addItemToFirebase(result);
-    getTrailer(movieID);//!!!!!!!!!!!!добавляет ютуб ролик
+    // addItemToFirebase(result);
+    getTrailer(movieID); //!!!!!!!!!!!!добавляет ютуб ролик
     //Показываем фоновый постер с оверлеем
     overlayBackgroundRef.classList.add('is-open');
     overlayRef.classList.add('is-open');
-    
+
     overlayBackgroundRef.style.backgroundImage = `linear-gradient(rgb(255, 255, 255, 0.1), rgb(255, 255, 255, 0.1)), url("${result.backdrop}")`;
     //Проверяем, есть ли текущий фильм в библиотеке, если да - делаем активными соотв. кнопки
     setButtonView(movieID, modalСardRef.querySelector('#btn-add-watched'));
@@ -82,20 +60,13 @@ function onModalBtnClick(e) {
     if (e.target.classList.contains('btn--active'))
       deleteItemFromLibrary(e.target.dataset.lib, movieID);
     else addItemToLibrary(e.target.dataset.lib);
-    addItemToLibraryFirebase(e.target.dataset.lib);
+    // addItemToLibraryFirebase(e.target.dataset.lib);
     setButtonView(movieID, e.target);
   }
 }
 //localStorage add
 function addItemToLocalStorage(res) {
   localStorage.setItem('ky', JSON.stringify(res));
-}
-//firebase add + remove
-function addItemToFirebase(result) {
-  set(ref(db, 'ky'), result);
-}
-function removeItemToFirebase() {
-  remove(ref(db, 'ky'));
 }
 //localStorage addItemToLibrary
 function addItemToLibrary(collection) {
@@ -104,17 +75,7 @@ function addItemToLibrary(collection) {
   arrLib.push(JSON.parse(localStorage.getItem('ky')));
   localStorage.setItem(collection, JSON.stringify(arrLib));
 }
-//firebase addItemToLibrary
-function addItemToLibraryFirebase(collection) {
-  let arr = [];
-  get(ref(db, 'ky')).then(data => {
-    // console.log(data.val());
-    arr.push(data.val());
-    // console.log('массив из базы', arr);
-    set(ref(db, collection), arr).then(); //data => console.log(data));
-  });
 
-}
 function deleteItemFromLibrary(collection, id) {
   let arrLib = JSON.parse(localStorage.getItem(collection));
   let arrRes = JSON.stringify(arrLib.filter(el => el.id !== id));
@@ -143,6 +104,5 @@ function closeModal() {
   overlayRef.removeEventListener('click', closeModal);
   window.removeEventListener('keydown', closeModal);
   localStorage.removeItem('ky');
-  removeItemToFirebase();
-  // if (libraryLink.classList.contains('active')) changeStorage(currentStorage);//!!!!!
+  // if (libraryLink.classList.contains('active')) changeStorage(currentStorage); //!!!!!
 }
