295427bdf393a65050b0a25fe546793d4a3f27a9
Ann456 <popovaa769@gmail.com>
2022-01-20 12:24:18 +0200

    add

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -20,27 +20,10 @@ openTeamModal.addEventListener('click', e => {
   });
 });
 
+// const btnModal = document.querySelector(".btn-modal");
+// console.log(btnModal);
 
 
 
 
-const modal = document.getElementById('myModal');
-const btn = document.getElementById('myBtn');
-const span =document.getElementsByClassName('close')
-console.log(btn);
-
-btn.onclick = function () {
-    modal.style.display="block";
-}
-
-span.onclick = function () {
-    modal.style.display='none';
-}
-
-window.onclick = function (e) {
-    if (e.target == modal) {
-        modal.style.display='none';
-    }
-    window.open(`mailto:popovaa769@gmail.com?subject=Message for Anna&body=Message: bhbhbhbh`)
-}
 
